212fb125ac946b80433f643bc94753936a191023
Subhadevik <146015926+Subhadevik@users.noreply.github.com>
2024-09-17 18:33:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -49,5 +49,5 @@ def analyze():
     prediction = loaded_model.predict(user_vector)
     return jsonify({'sentiment': str(prediction[0])})
 
-if __name__ == "__main__":
-    app.run(debug=True)
+#if __name__ == "__main__":
+    #app.run(debug=True)
